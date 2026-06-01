7c4982400563f53e5b37cd44d8590af6cc9fe392
Harendra Tyagi <156878636+Harendratyagi18@users.noreply.github.com>
2026-06-01 16:27:29 +0530

    Update author information in FileSizeViewer.py

--- FileSizeViewer.py
+++ FileSizeViewer.py
@@ -1,6 +1,6 @@
-# Author: Kartikey Baghel
-# E-mail: kartikeybaghel@hotmail.com
-# Github: https://github.com/piyushk789
+# Author: Harendra Tyagi
+# Email : harendra.tyagi_cs22@gla.ac.in
+# Github: https://github.com/Harendratyagi18/File-Size-Viewer
 
 import os
 from tkinter import filedialog, messagebox, ttk
